1e27db3f8b479468c41774779b0096d6f28d3c4d
akari263 <30289445+akari263@users.noreply.github.com>
2025-05-21 11:20:04 +0800

    Update d2550-1.sh

--- d2550-1.sh
+++ d2550-1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git gecoosac https://github.com/lwb1978/openwrt-gecoosac' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
